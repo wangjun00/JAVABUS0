73943d6d7b51fc65e9bfc3aa3d38ec6d67484b10
wangjun00 <964370140@qq.com>
2017-03-21 08:56:14 +0800

    this is
    Signed-off-by: wangjun00 <964370140@qq.com>

---
+++ JavaBase/src/com/wangjun00/javacore/TestType.java
@@ -0,0 +1,24 @@
+package com.wangjun00.javacore;
+
+      class TestType {
+       public static void main(String[ ] args) {
+    	   
+    	   
+    	   double score;
+    	   score=98.5;
+    	 /* double score = 98.5;   */ 
+    	   
+    	   String name;
+    	   name="三胖";
+             /* String name = "三胖";*/
+    	   
+              char sex;
+              sex='男';
+            /*  char sex = '男';*/
+    
+   	       System.out.println("本次考试成绩最高分： " + score);
+	       System.out.println("最高分得主： " + name);
+	       System.out.println("性别： " + sex);
+	       
+       }
+}
